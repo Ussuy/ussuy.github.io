80cd379a0a7d80f3469edc68c870204af057e357
Ussuy <ussuyart@gmail.com>
2020-05-05 22:49:23 -0300

    added vue cdn and webpack build

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import 'css/styles.scss'
+const vm = new Vue({
+  el: '#app',
 
+  data: {
+    message: 'Hello world'
+  },
+  template: `
+    <main class="profile">
+      <div class="profile_photo"></div>
+      <h1 class="profile_title">Arthur Ussuy Ferrari</h1>
+      <h2 class="profile_subtitle">Front-end Developer</h2>
+    </main>
+  `
+})  
