19dcd2140797d5e80cde6f0f26efb1eccea4a9cd
Ussuy <ussuyart@gmail.com>
2020-05-05 22:41:23 -0300

    adjustments

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path =  require('path');
+const extract = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'eval-cheap-module-source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }, {
+                test: /\.(png|svg|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: extract.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new extract({
+            filename: 'bundle.css'
+        })
+    ],
+    resolve: {
+        alias: {
+            css: path.resolve(__dirname, 'src/css'),
+            image: path.resolve(__dirname, 'src/images'),
+        },
+    },
+    mode: 'development'
+}
