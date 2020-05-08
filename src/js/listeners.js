5d085c39bf3e1a462dbdbe5f0be9e6f4a61b64ad
Ussuy <ussuyart@gmail.com>
2020-05-08 00:12:18 -0300

    function to get html elements added

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -2,9 +2,24 @@ export default () => {
   window.addEventListener('wheel', wheeling, false)
 
   function wheeling() {
-    console.log('wheeling...')
+    const HTMLElements = getHTMLElements()
+    HTMLElements.map(element => {
+      // trocar classe TODO
+    })
   }
 
+  function getHTMLElements() {
+
+    const getElements = (element) => {
+      return element.innerText !== undefined
+    }
+
+    return Object.values(document.getElementById('main')
+    .childNodes)
+    .filter(getElements)
+  }
+  
 
 
+  
 }
